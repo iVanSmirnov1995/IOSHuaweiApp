6d54818b36ebcc7a98393150b0a65c746a9f35aa
Leonid Serebryanyy <leosilver@yandex.ru>
2017-03-29 04:37:27 +0300

    12345

---
+++ IOSHuaweiApp/SupportModel.m
@@ -0,0 +1,13 @@
+//
+//  SupportModel.m
+//  IOSHuaweiApp
+//
+//  Created by 1 on 28.03.17.
+//  Copyright © 2017 Smirnov Ivan. All rights reserved.
+//
+
+#import "SupportModel.h"
+
+@implementation SupportModel
+
+@end
